f5091eb1832f032fdbf077f06f375043aab7cdd0
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2023-12-18 05:02:18 +0300

    Initial commit

---
+++ src/test/java/net/javademo/employeemanagementsystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javademo.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
