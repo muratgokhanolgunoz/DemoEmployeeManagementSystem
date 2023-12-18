f3ca10d259beaee31b1e8531a83bcfb5fb7ed8ef
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2023-12-18 06:06:29 +0300

    Create EmployeeDto and EmployeeMapper classes

---
+++ src/main/java/net/javademo/employeemanagementsystem/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package net.javademo.employeemanagementsystem.mapper;
+
+import net.javademo.employeemanagementsystem.dto.EmployeeDto;
+import net.javademo.employeemanagementsystem.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
